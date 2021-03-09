00595578ec5b58138854935f2748b333fd39ecbd
Joseph Budina <joebud3@gmail.com>
2021-03-09 09:53:41 -0700

    Add first story

---
+++ spec/features/competition/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+describe 'Competition Index Page' do
+  it 'Lists comp names as links to show page' do
+    @regional = Competition.create(name: "Regional", location: "Chicago?", sport: "ball sport")
+    @state = Competition.create(name: "State", location: "London", sport: "ball sport")
+    visit competitions_path
+
+    expect(page).to have_link('Regional')
+    expect(page).to have_link('State')
+
+    click_link 'Regional'
+
+    expect(current_path).to eq(competition_path(@regional))
+  end
+end
